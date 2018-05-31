7d3d1f14a108aac81f3f65cb8f0e72ec06ac5acc
jonathasmedina <emaildojonathas@gmail.com>
2018-05-31 15:34:34 -0400

    -

--- app/src/main/java/com/example/jonathas/splatics/MainActivity.java
+++ app/src/main/java/com/example/jonathas/splatics/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.jonathas.splatics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bt = (Button) findViewById(R.id.button2);
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Tela2.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/jonathas/splatics/Tela2.java
+++ app/src/main/java/com/example/jonathas/splatics/Tela2.java
@@ -1,13 +1,42 @@
 package com.example.jonathas.splatics;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Tela2 extends AppCompatActivity {
+
+    Button bt2;
+    Button bt3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela2);
+
+        bt2 = findViewById(R.id.button3);
+        bt3 = findViewById(R.id.button4);
+
+        bt2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tela2.this, Tela2.class);
+                startActivity(intent);
+            }
+        });
+
+        bt3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Tela2.this, Tela3.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+
     }
 }
